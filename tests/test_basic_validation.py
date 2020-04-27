c0e536d87a0289945b026059af9a9a33a17abeed
Evgeniy Burdin <e.s.burdin@yandex.ru>
2020-04-27 12:52:04 +0400

    Сделал папку тестов

---
+++ tests/test_basic_validation.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+from data_classes import BasicValidation
+
+
+def test__post_init__():
+    """
+        Тест запуска валидации после создания экземпляра
+        (в методе self.__post_init__())
+    """
+    @dataclass
+    class FakeBasicValidation(BasicValidation):
+        """
+            Подменим метод старта валидаци
+        """
+        def _run_validation(self):
+            self.validation_run = True
+
+    instance = FakeBasicValidation()
+    assert instance.validation_run
