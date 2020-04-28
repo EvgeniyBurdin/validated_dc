3274643cb754d361f98d8955ba90f73da0099978
Evgeniy Burdin <e.s.burdin@yandex.ru>
2020-04-28 09:25:10 +0400

    Добавил тесты базового класса на создание валидного и невалидного экземпляра

--- tests/test_basic_validation.py
+++ tests/test_basic_validation.py
@@ -3,6 +3,46 @@ from dataclasses import dataclass
 from validated_dc import BasicValidation
 
 
+class СustomСlass:
+    foo = 'foo'
+
+    def bar(self):
+        pass
+
+
+@dataclass
+class Foo(BasicValidation):
+    i: int
+    s: str
+    l: list
+    cc: СustomСlass
+
+
+correct_input = {
+    'i': 1,
+    's': '2',
+    'l': [3, '4'],
+    'cc': СustomСlass()
+}
+
+
+def test_basic_validation_valid():
+
+    instance = Foo(**correct_input)
+
+    assert instance.get_errors() is None
+
+
+def test_basic_validation_not_valid():
+
+    nocorrect_input = correct_input
+    nocorrect_input['s'] = 2
+
+    instance = Foo(**nocorrect_input)
+
+    assert instance.get_errors()
+
+
 def test__post_init__():
     """
         Тест запуска валидации после создания экземпляра
