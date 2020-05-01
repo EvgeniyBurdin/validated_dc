61413cbc7125c0600024dbbed6e67ac36eb9ed97
Evgeniy Burdin <e.s.burdin@yandex.ru>
2020-05-01 17:58:04 +0400

    Сделал тесты для _try_replacing()

--- tests/test_instance_validation.py
+++ tests/test_instance_validation.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass,fields
+from dataclasses import dataclass, fields
 
 from validated_dc import InstanceValidation
 
@@ -58,9 +58,72 @@ def test_init_field_validation():
     assert instance._replacement is None
 
 
-'''
-    # Если это свойство установлено в True, то, при успехе валидации, если
-    # значение поля - словарь, а анотация к полю - класс-потомок
-    # InstanceValidation, будет произведена замена словаря на экземпляр класса
-    # из аннотации.
-'''
+def test_try_replacing_successfully():
+    """
+        Тест вызова _try_replacing() который завершился заменой значения
+    """
+    # Создадим экземпляр с полем, которое имеет аннотацию
+    # датаклассом-потомком InstanceValidation,
+    instance = Bar(foo=Foo(i=1))
+
+    # Изменим значение поля foo на словарь (на самом деле, здесь, в тесте,
+    # можно подставить любое значение, но при использовании класса это
+    # будет именно словарь)
+    data = {'i': 2}
+    instance.foo = data
+
+    # Для наглядности, присвоим полю self._replace значение True
+    # (это значение по умолчанию, но именно оно необходимо для возможности
+    # произвести замену)
+    instance._replace = True  # Выполнять замену, если есть на что
+
+    # В поле self._replacement должен быть подготовленный экземпляр, который
+    # был получен из словаря
+    instance._replacement = Foo(**data)
+
+    # Вызовем метод
+    instance._try_replacing()
+
+    # Должна произойти замена значения поля
+    assert instance.foo == instance._replacement
+
+
+def test_try_replacing_unsuccessfully():
+    """
+        Тест вызова _try_replacing() который НЕ завершился заменой значения
+    """
+    # Создадим экземпляр с полем, которое имеет аннотацию
+    # датаклассом-потомком InstanceValidation,
+    instance = Bar(foo=Foo(i=1))
+
+    # Изменим значение поля foo на словарь (на самом деле, здесь, в тесте,
+    # можно подставить любое значение, но при использовании класса это
+    # будет именно словарь)
+    data = {'i': 2}
+    instance.foo = data
+
+    # 1. Присвоим полю self._replace значение False
+    instance._replace = False  # Не выполнять замену
+
+    # В поле self._replacement должен быть подготовленный экземпляр, который
+    # был получен из словаря
+    instance._replacement = Foo(**data)
+
+    # Вызовем метод
+    instance._try_replacing()
+
+    # Значения поля должно остаться прежним
+    assert instance.foo == data
+
+    # 2. Присвоим полю self._replace значение True
+    instance._replace = True
+
+    # Но в поле self._replacement поставим None (значение, которое оно
+    # получает после инициализации перед валидацией поля)
+    instance._replacement = None  # Нет значения для замены
+
+    # Вызовем метод
+    instance._try_replacing()
+
+    # Значения поля должно остаться прежним
+    assert instance.foo == data
