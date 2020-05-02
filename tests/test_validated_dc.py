e3c1305c55cde74abe8854048ad6cb4646d4db62
Evgeniy Burdin <e.s.burdin@yandex.ru>
2020-05-02 19:31:33 +0400

    Сделал тест get_nested_validated_dc()

---
+++ tests/test_validated_dc.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from typing import List, Literal, Optional, Union
+
+from validated_dc import ValidatedDC
+
+
+@dataclass
+class Phone(ValidatedDC):
+    phone: str
+    kind: Literal['personal', 'work'] = 'personal'
+
+
+@dataclass
+class Email(ValidatedDC):
+    email: str
+    kind: Literal['personal', 'work'] = 'work'
+
+
+@dataclass
+class Address(ValidatedDC):
+    city: str
+    zip_code: Optional[str] = None
+
+
+@dataclass
+class Person(ValidatedDC):
+    name: str
+    age: int
+    contact: Union[Phone, Email, List[Union[Phone, Email]]]
+    address: Address
+
+
+@dataclass
+class Workers(ValidatedDC):
+    person: Union[Person, List[Person]]
+
+
+def test_get_nested_validated_dc():
+    """
+        Тест метода-класса get_nested_validated_dc().
+        Он должен вернуть множество классов-потомков ValidatedDC, которые
+        вложены в определениях полей текущего класса.
+    """
+    nested_validated_dc = set((
+        Phone, Email, Address, Person
+    ))
+
+    assert Workers.get_nested_validated_dc() == nested_validated_dc
