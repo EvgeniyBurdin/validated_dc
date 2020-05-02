0f9c2728784bea5dab8936bc8d1740679ed36f7f
Evgeniy Burdin <e.s.burdin@yandex.ru>
2020-05-02 16:46:00 +0400

    Сделал тесты вспомогательных методов и метода _is_union_instance()

--- tests/test_typing_validation.py
+++ tests/test_typing_validation.py
@@ -2,9 +2,11 @@
     Тесты класса TypingValidation.
 """
 from dataclasses import dataclass
-from typing import List, Literal, Optional, Union, Any, Dict
+from typing import Any, Dict, List, Literal, Optional, Union
 
-from validated_dc import TypingValidation
+import pytest
+
+from validated_dc import STR_ALIASES, TypingValidation
 
 
 @dataclass
@@ -34,13 +36,18 @@ class Person(TypingValidation):
     extra: Any
 
 
-def test_is_instance_true():
+@pytest.fixture()
+def instance():
+    """
+        Возвращает валидный экземаляр Phone
+    """
+    return Phone(phone='79991112233')
+
+
+def test_is_instance_true(instance):
     """
         Тест метода _is_instance() который вернет True
     """
-    # Возьмем любой экземпляр
-    instance = Phone(phone='79991112233')
-
     annotation = Any  # Возьмем любой поддерживаемый алиас
     value = 1
     assert instance._is_instance(value, annotation)
@@ -70,13 +77,10 @@ def test_is_instance_true():
     assert instance._is_instance(value, annotation)
 
 
-def test_is_instance_false():
+def test_is_instance_false(instance):
     """
         Тест метода _is_instance() который вернет False
     """
-    # Возьмем любой экземпляр
-    instance = Phone(phone='79991112233')
-
     annotation = List[int]
     value = ['1', ]  # Ошибка - str вместо int
     assert not instance._is_instance(value, annotation)
@@ -101,7 +105,7 @@ def test_is_instance_false():
     assert not instance._is_instance(value, annotation)
 
 
-def test_is_instance_false_error_with_exception():
+def test_is_instance_false_error_with_exception(instance):
     """
         Тест метода _is_instance() который вернет False, и в ошибке будет
         исключение.
@@ -109,9 +113,6 @@ def test_is_instance_false_error_with_exception():
         Этот метод "гасит" все исключения, но если оно случилось, то
         записывает информацию о нем в ошибку.
     """
-    # Возьмем любой экземпляр
-    instance = Phone(phone='79991112233')
-
     # Убедимся что ошибок нет
     assert not instance._field_errors
 
@@ -123,3 +124,68 @@ def test_is_instance_false_error_with_exception():
     # ... а список ошибок должен пополниться одной ошибкой, которая,
     # в том числе, имеет и информацию об исключении
     assert instance._field_errors[0].exception
+
+
+def test_is_typing_alias(instance):
+    """
+        Тест метода _is_typing_alias() который возвращает True если
+        аннотация - из модуля typing, и False если нет.
+    """
+    # Метод должен вернуть True
+    annotation = str(List[int])  # Поддерживаемый алиас из typing
+    assert instance._is_typing_alias(annotation)
+    annotation = str(Dict[int, int])  # НЕ поддерживаемый алиас из typing
+    assert instance._is_typing_alias(annotation)
+
+    # Метод должен вернуть False
+    annotation = str(int)
+    assert not instance._is_typing_alias(annotation)
+    annotation = str(Email)
+    assert not instance._is_typing_alias(annotation)
+
+
+def test_is_supported_alias(instance):
+    """
+        Тест метода _is_supported_alias() который возвращает True если
+        аннотация из модуля typing поддерживается валидацией класса, и
+        False если нет.
+    """
+    for str_alias in STR_ALIASES.values():
+        assert instance._is_supported_alias(str_alias)
+
+
+def test_is_union_instance(instance):
+    """
+        Тест метода _is_union_instance() который возвращает True если
+        значение имеет тип одного из типов кортежа Union, и False если нет.
+    """
+    annotation = Union[int, str]
+
+    value = 1
+    assert instance._is_union_instance(value, annotation)
+    value = '2'
+    assert instance._is_union_instance(value, annotation)
+    value = 0.3  # Ошибка - float не int
+    assert not instance._is_union_instance(value, annotation)
+
+    # Union так же может иметь вложенные поддерживаемые алиасы из typing
+    annotation = Union[List[int], str]
+    value = [1, 2, ]
+    assert instance._is_union_instance(value, annotation)
+    value = '1'
+    assert instance._is_union_instance(value, annotation)
+    value = [1, '2', ]   # Ошибка - элемент списка может быть только int
+    assert not instance._is_union_instance(value, annotation)
+
+    # Union сам может быть вложенныи в другой поддерживаемый алиас из typing
+    annotation = Union[List[Union[int, str]], str]
+    value = [1, '2', ]
+    assert instance._is_union_instance(value, annotation)
+    value = [1, '2', 0.3]  # Ошибка - элемент списка может быть int или str
+    assert not instance._is_union_instance(value, annotation)
+
+    # Так как TypingValidation наследник InstanceValidation, то следующий
+    # вызов так же вернет True
+    annotation = Union[List[Union[int, Email]], str]
+    value = [1, {'email': 'mail@mail.com'}, 2, Email(email='mail2@mail.com')]
+    assert instance._is_union_instance(value, annotation)
